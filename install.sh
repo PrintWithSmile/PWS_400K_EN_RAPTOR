9928abeaae8014e022db73718d9faa3ce7ed3f12
Print With Smile <123603173+PrintWithSmile@users.noreply.github.com>
2023-12-06 21:28:33 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,35 @@
+cd /home/pi/PWS
+sudo git clone https://github.com/PrintWithSmile/PWS_400K_EN_RAPTOR.git
+
+
+sudo nano /home/pi/PWS/PWS_400K_EN_RAPTOR/.git/hooks/post-merge
+
+#!/bin/sh
+rm /home/pi/PWS/update_conf.sh
+cp -f /home/pi/PWS/PWS_400K_EN_RAPTOR/update.sh /home/pi/PWS/update_conf.sh
+cd /home/pi/PWS
+chmod +x update_conf.sh 
+./update_conf.sh
+
+sudo chmod +x /home/pi/PWS/PWS_400K_EN_RAPTOR/.git/hooks/post-merge
+
+sudo nano /home/pi/PWS/update_conf.sh
+
+xxxxx
+
+sudo chmod +x /home/pi/PWS/update_conf.sh
+
+cd /home/pi/printer_data/config/
+sudo mkdir /home/pi/printer_data/config/PWS_config
+sudo mkdir /home/pi/printer_data/config/Archive
+sudo rm moonraker.conf
+sudo rm crowsnest.conf
+sudo rm printer.cfg
+sudo cp /home/pi/PWS/PWS_400K_EN_RAPTOR/printer.cfg /home/pi/printer_data/config/
+sudo cp /home/pi/PWS/PWS_400K_EN_RAPTOR/crowsnest.conf /home/pi/printer_data/config/
+sudo cp /home/pi/PWS/PWS_400K_EN_RAPTOR/moonraker.conf /home/pi/printer_data/config/
+sudo cp /home/pi/PWS/PWS_400K_EN_RAPTOR/Configurations/* /home/pi/printer_data/config/PWS_config/
+sudo chown -R pi:pi /home/pi/PWS
+sudo chown -R pi:pi /home/pi/printer_data/config
+sudo service klipper restart
+sudo service moonraker restart
